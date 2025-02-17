a8eb4dd5d3f00f7cf39472781878146798fc0120
Dominika Warchoł <242408@student.pwr.edu.pl>
2025-02-17 17:28:10 +0100

    Initial commit

---
+++ src/app.routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from "@angular/router";
+import {TaskComponent} from "./app/tasks/task/task.component";
+
+export const appRoutes: Routes = [
+  {
+    path: 'tasks',
+    component: TaskComponent
+  },
+
+]
